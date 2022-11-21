f098131131be3881d01645a5bb9c0983c0635fd4
Rustambek2003 <rustambekjumaqulov@gmail.com>
2022-11-21 19:18:32 +0500

    'get_user_names_with_age_range_and_country'

--- get_user_names_with_age_range_and_country.py
+++ get_user_names_with_age_range_and_country.py
@@ -11,4 +11,10 @@ def get_user_names_with_age_range_and_country(data:list, min_age:int, max_age:in
     Returns:
         list: A list of users with the given age range and country
     """
-    pass
+    age1 = data[0]['age']
+    age2 = data[1]['age']
+    if min_age<age1<max_age:
+        return [data[0]["name"]]
+    elif max_age<age2<max_age:
+        return [data[1]["name"]]
+print(get_user_names_with_age_range_and_country([{'name': 'John', 'age': 27}, {'name': 'Mary', 'age': 42}],20,30,"USA"))
