95bf37f91a1a5dfc0b93b063aeb6393d5c8e0fa6
Rustambek2003 <rustambekjumaqulov@gmail.com>
2022-11-21 19:36:35 +0500

    'sum_age_value

--- sum_age_value.py
+++ sum_age_value.py
@@ -6,4 +6,7 @@ def sum_age_values(data:list) -> int:
     Returns:
         int: The sum of all age values in the dictionary
     """
-    pass
+    age1 = data[0]['age']
+    age2 = data[1]['age']
+    return age1 + age2
+print(sum_age_values([{'name': 'John', 'age': 27}, {'name': 'Mary', 'age': 42}]))
