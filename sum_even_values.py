3a24fa3731a097350a4a8958fa1e116f222ae09d
Rustambek2003 <rustambekjumaqulov@gmail.com>
2022-11-21 19:41:30 +0500

    sum_even_values

--- sum_even_values.py
+++ sum_even_values.py
@@ -6,4 +6,9 @@ def sum_even_values(data: dict) -> int:
     Returns:
         int: The sum of all even values in the dictionary
     '''
-    pass
+    ans = 0
+    for i in data.values():
+        if i % 2 == 0:
+            ans = i
+    return ans
+print(sum_even_values({'a': 1, 'b': 2, 'c': 3}))
