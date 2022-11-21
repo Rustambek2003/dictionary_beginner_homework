0788812436b41d7f627acea4e4c182ff26d5ac4b
Rustambek2003 <rustambekjumaqulov@gmail.com>
2022-11-21 18:58:36 +0500

    'get_country_with_most_users_with_age'

--- get_country_with_most_users_with_age.py
+++ get_country_with_most_users_with_age.py
@@ -8,4 +8,10 @@ def get_country_with_most_users_with_age(data:list, age:int) -> str:
     Returns:
         str: The country with the most users with the given age
     """
-    pass
+    age1 = data[0]['age']
+    age2 = data[1]['age']
+    if age == age1:
+        return data[0]['country']
+    elif age == age2:
+        return data[1]['country']
+print(get_country_with_most_users_with_age([{'name': 'John', 'country': 'USA', 'age': 27}, {'name': 'Mary', 'country': 'UK', 'age': 42}],27))
