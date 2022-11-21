c5120bd098ca5e2bbff1b30aa558fb33186aaf97
Rustambek2003 <rustambekjumaqulov@gmail.com>
2022-11-21 19:01:50 +0500

    'get_country_with_most_users'

--- get_country_with_most_users.py
+++ get_country_with_most_users.py
@@ -7,4 +7,11 @@ def get_country_with_most_users(data:list) -> str:
     Returns:
         str: The country with the most users
     """
-    pass
+    country1 = data[0]['country']
+    country2 = data[1]['country']
+    if len(country1) > len(country2):
+        return country1
+    else:
+        return country2
+    return 
+print(get_country_with_most_users([{'name': 'John', 'country': 'USA'}, {'name': 'Mary', 'country': 'UK'}]))
