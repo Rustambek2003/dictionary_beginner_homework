978d1b64d262eff7723533a48c01e7de58277e8d
Rustambek2003 <rustambekjumaqulov@gmail.com>
2022-11-21 19:34:51 +0500

    'get_user_names'

--- get_user_names.py
+++ get_user_names.py
@@ -8,4 +8,10 @@ def get_user_names(data:list, country:str) -> list:
     Returns:
         list: A list of users with the given country
     """
-    pass
+    country1 = data[0]['country']
+    country2 = data[1]['country']
+    if country1 == country:
+        return [data[0]["name"]]
+    elif country2 == country:
+        return [data[1]["name"]]
+print(get_user_names([{'name': 'John', 'country': 'USA'}, {'name': 'Mary', 'country': 'UK'}],"USA"))
